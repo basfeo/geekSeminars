7b1fa872b9927d62c2f5ab459e0ec8bf5e917262
Vasily <48855460+basfeo@users.noreply.github.com>
2023-07-10 15:10:40 +0300

    Added homeworks for 9 seminars

---
+++ Seminar9/HWtask66/Program.cs
@@ -0,0 +1,22 @@
+﻿int SumNaturalNumbers(int m, int n)
+{
+    if (m > n)
+        return 0;
+    else
+        return m + SumNaturalNumbers(m + 1, n);
+}
+
+void InOutResult(string text)
+{
+    Console.Write("Введите значение M: ");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите значение N: ");
+    int n = int.Parse(Console.ReadLine());
+
+    int sum = SumNaturalNumbers(m, n);
+    Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {sum}");
+    Console.ReadLine();
+}
+
+InOutResult("Imput num");
---
+++ Seminar9/HWtask68/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значения m и n:");
+
+        int m = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
